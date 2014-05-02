fc255b1f8ac9b592b4959814cf65b83ab7c212e7
Michal Petrucha <michal.petrucha@ksp.sk>
2014-05-02 15:39:32 +0200

    The runner now tries substantially more parameter cominations.
    Lowered the number of attempts per combo.

--- code/testrun.sh
+++ code/testrun.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
 
-MAX_DENSITY=0.65
-SEQUENCES=1000
-INSTANCES_PER_SEQUENCE=500
-STRATEGY_ARGS="--strategy random"
+DENSITIES=".645 .6 .5 .4 .3"
+SEQUENCE_SIZES="10 20 30 40 50 60 70"
+SEQUENCES=100
+INSTANCES_PER_SEQUENCE=100
+# List of strategy combos to run:
+# strategy,output dir suffix
+# This needs to end with a semicolon, otherwise sh won't pick up the last
+# element.
+STRATEGY_COMBOS="--strategy random,random;\
+                 --strategy linear-dep --linear-deps 1,linear-dep/1;\
+                 --strategy linear-dep --linear-deps 1,linear-dep/5;"
 
-for elems in 05 10 15 20; do
-    echo '**********************'
-    echo "Running for $elems elements"
-    echo '**********************'
-    mkdir -p results/$elems
-    seq -w $SEQUENCES | \
-        parallel --progress \
-            ./find_unsolvable.py --density $MAX_DENSITY \
-                --elements $elems --instances $INSTANCES_PER_SEQUENCE \
-                $STRATEGY_ARGS \
-                ">results/$elems/{}"
+OUTDIR_TEMPLATE="results/density%.03f/n%02d/%s"
+
+for MAX_DENSITY in $DENSITIES; do
+    for ELEMS in $SEQUENCE_SIZES; do
+        echo "$STRATEGY_COMBOS" | while IFS=',' read -d ';' STRATEGY_ARGS OUTDIR_SUFFIX; do
+            echo '***********************'
+            printf "Running for density %.03f, %02d elements, strategy %s\n" \
+                    "$MAX_DENSITY" "$ELEMS" "$STRATEGY_ARGS"
+            echo '***********************'
+            OUTDIR="$(printf "$OUTDIR_TEMPLATE" "$MAX_DENSITY" "$ELEMS" "$OUTDIR_SUFFIX")"
+            mkdir -p "$OUTDIR"
+            seq -w $SEQUENCES | \
+                parallel --progress \
+                    ./find_unsolvable.py --density $MAX_DENSITY \
+                        --elements $ELEMS --instances $INSTANCES_PER_SEQUENCE \
+                        $STRATEGY_ARGS \
+                        ">$OUTDIR/{}"
+        done
+    done
 done
